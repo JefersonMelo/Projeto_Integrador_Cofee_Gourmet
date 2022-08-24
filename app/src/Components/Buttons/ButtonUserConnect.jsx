6750b6ac7059b7acb2b15cc5ff4d1f235c16f072
Jeferson Melo <jefsdemelo@gmail.com>
2022-08-24 20:01:39 -0300

    Feat(ButtonUserConnect.jsx): initial component

---
+++ app/src/Components/Buttons/ButtonUserConnect.jsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import PersonIcon from "@mui/icons-material/Person";
+
+export default function ButtonUserConnect() {
+  return (
+    <Box>
+      <Button color="inherit">
+        <PersonIcon />
+      </Button>
+    </Box>
+  );
+}
