153f964c5f97cea2c29959da8d886f2cc56a610f
Jeferson Melo <jefsdemelo@gmail.com>
2022-08-27 21:52:41 -0300

    Fix(Home.jsx): Deixa de receber um component.
    OBS: Ainda sem uso

--- app/src/Pages/Home.jsx
+++ app/src/Pages/Home.jsx
@@ -1,18 +1,9 @@
 import * as React from "react";
-import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
-import NavBar from "../Components/NavBar/NavBar";
-import LeftDrawer from "../Components/Drawers/LeftDrawer";
 
 export default function Home() {
   return (
     <>
-      <LeftDrawer />
       <Box
         sx={{
           display: "flex",
@@ -21,9 +12,7 @@ export default function Home() {
           alignItems: "center",
         }}
       >
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          Home
-        </Typography>
+        Home Usuário Conectado
       </Box>
     </>
   );
