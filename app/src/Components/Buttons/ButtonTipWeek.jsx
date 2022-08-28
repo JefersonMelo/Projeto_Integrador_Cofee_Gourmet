541c9cde579e48b1f636c2bbf862049c3c63e9b9
Jeferson Melo <jefsdemelo@gmail.com>
2022-08-27 21:36:38 -0300

    Feat(ButtonTipWeek): Novo Componente redireciona para o blog
    Obs: Não é necessário estar autenticado

---
+++ app/src/Components/Buttons/ButtonTipWeek.jsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+
+export default function ButtonTipWeek() {
+  return <Button variant="contained">Acesse Nosso Blog Com Dicas da Semana</Button>;
+}
