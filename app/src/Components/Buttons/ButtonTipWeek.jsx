0d16c85b123ba5d2dd25275e6b9afb08f389253e
Jeferson Melo <jefsdemelo@gmail.com>
2022-08-29 18:05:44 -0300

    Feat(Tip Week): Botão redireciona para o blog sobre café

--- app/src/Components/Buttons/ButtonTipWeek.jsx
+++ app/src/Components/Buttons/ButtonTipWeek.jsx
@@ -1,6 +1,14 @@
 import * as React from "react";
 import Button from "@mui/material/Button";
+import { openInNewTab } from "../../Helpers/Helpers";
 
 export default function ButtonTipWeek() {
-  return <Button variant="contained">Acesse Nosso Blog Com Dicas da Semana</Button>;
+  return (
+    <Button
+      variant="contained"
+      onClick={() => openInNewTab("http://blog.clubecafe.net.br/")}
+    >
+      Acesse Nosso Blog Com Dicas Sobre Café
+    </Button>
+  );
 }
