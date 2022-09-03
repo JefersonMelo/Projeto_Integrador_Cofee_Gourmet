230bb314303c5f0550b6daf9c0d756f142b678cb
Jeferson Melo <jefsdemelo@gmail.com>
2022-09-03 16:04:49 -0300

    Fix(favicon.ico): Novo favicon.ico

---
+++ app/src/Components/NavBar/NavBarLogo.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import { Link } from "react-router-dom";
+import { Theme } from "../../Helpers/Theme";
+
+export const NavBarLogo = () => {
+  const colors = Theme.palette;
+  return (
+    <Box
+      component={Link}
+      sx={{
+        flexGrow: 1,
+        position: "relative",
+        textDecoration: "none",
+        color: colors.dropzone.grey98,
+      }}
+      to={{ pathname: "/" }}
+    >
+      <Typography variant="h6" component="div">
+        COFFEE AT HOME
+      </Typography>
+    </Box>
+  );
+};
