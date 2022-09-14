dd46613d329f32b1fe99e34ef7a324401cb7f6d7
Jeferson Melo <jefsdemelo@gmail.com>
2022-09-14 20:46:35 -0300

    Feat(home.jsx): configuração inicial

--- app/src/Pages/Home.jsx
+++ app/src/Pages/Home.jsx
@@ -1,19 +1,26 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
+import { Typography } from "@mui/material";
+import { Main } from "../Pages/Config/MainStyle";
+import { useAppContext } from "../Contexts/AppContext";
+
 
 export default function Home() {
+  const [appContext, ] = useAppContext();
   return (
-    <>
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        Home Usuário Conectado
-      </Box>
-    </>
+    <Box sx={{ mt: 7, flexGrow: 12 }}>
+    <Main open={appContext.drawerOpened} context={appContext}>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Typography variant="h5">Home Usuário Conectado</Typography>
+    </Box>
+    </Main>
+    </Box> 
   );
 }
