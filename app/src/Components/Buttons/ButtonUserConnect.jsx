a149e27afc8135e7fd5a81a5dc200960ea55f955
Jeferson Melo <jefsdemelo@gmail.com>
2022-10-16 19:59:05 -0300

    Fix(info): add tooltip

--- app/src/Components/Buttons/ButtonUserConnect.jsx
+++ app/src/Components/Buttons/ButtonUserConnect.jsx
@@ -3,14 +3,22 @@ import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import PersonIcon from "@mui/icons-material/Person";
 import { useNavigate } from "react-router-dom";
+import { Tooltip } from "@mui/material";
 
 export default function ButtonUserConnect() {
   const navigate = useNavigate();
   return (
     <Box>
-      <Button color="inherit" onClick={() => navigate("/login")}>
-        <PersonIcon />
-      </Button>
+      <Tooltip 
+      arrow
+      title={"Conecte-se"}
+      >
+        <span>
+          <Button color="inherit" onClick={() => navigate("/login")}>
+            <PersonIcon />
+          </Button>
+        </span>
+      </Tooltip>
     </Box>
   );
 }
