547bac6d5da825e46641090e290969b4dd782015
Jeferson Melo <jefsdemelo@gmail.com>
2022-10-16 20:00:18 -0300

    Fix(ButtonAddShoppingCar): add tooltip e option clik
    inicio das configurações para add itens ao carrinho.

--- app/src/Components/Buttons/ButtonAddShoppingCart.jsx
+++ app/src/Components/Buttons/ButtonAddShoppingCart.jsx
@@ -1,14 +1,29 @@
 import * as React from "react";
+import { useNavigate } from "react-router-dom";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
-import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+import { useUserContext } from "../../Contexts/UserContext";
+import { Tooltip } from "@mui/material";
 
 export default function ButtonAddShoppingCart() {
+  const [userContext] = useUserContext();
+  const navigate = useNavigate();
   return (
     <Box>
-      <Button color="inherit">
-        <AddShoppingCartIcon color="inherit" />
-      </Button>
+      <Tooltip
+        arrow
+        title={!userContext.token ? "Conecte-se" : "Adicionar ao Carrinho"}
+      >
+        <span>
+          <Button
+            color="inherit"
+            onClick={() => (!userContext.token ? navigate("/login") : null)}
+          >
+            <AddShoppingCartIcon color="inherit" />
+          </Button>
+        </span>
+      </Tooltip>
     </Box>
   );
 }
