56335b28a98e53785d3f96829d5aa6b69e9ce4a8
Jeferson Melo <jefsdemelo@gmail.com>
2022-10-29 12:38:37 -0300

    fix(deploy heroku): error code=H10

--- wsgi.py
+++ wsgi.py
@@ -6,5 +6,4 @@ import app
 this_files_dir = os.path.dirname(os.path.obspath(__file__))
 os.chdir(this_files_dir)
 sys.stdout.reconfigure(encodings='utf-8')
-asyncio.Server(app)
-# , host='0.0.0.0', port=8999, url_scheme='http'
+asyncio.Server(app, host='0.0.0.0', port=8999, url_scheme='http')
