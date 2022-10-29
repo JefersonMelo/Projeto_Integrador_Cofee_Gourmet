8f9433593d15a9fea1392feef94fba4f43b83fbd
Jeferson Melo <jefsdemelo@gmail.com>
2022-10-28 22:38:39 -0300

    fix(home.py): error code=H10

--- api/routes/home.py
+++ api/routes/home.py
@@ -1,13 +1,13 @@
-# from fastapi import HTTPException
-# from fastapi import APIRouter
-#
-# router = APIRouter()
-#
-#
-# @router.get('/')
-# async def home():
-#     try:
-#         pass
-#     except Exception as e:
-#         msg = str(e)
-#         raise HTTPException(status_code=404, detail=msg)
+from fastapi import HTTPException
+from fastapi import APIRouter
+
+router = APIRouter()
+
+
+@router.get('/')
+async def home():
+    try:
+        return {'results': 'connect'}
+    except Exception as e:
+        msg = str(e)
+        raise HTTPException(status_code=404, detail=msg)
