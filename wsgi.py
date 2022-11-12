028ea9f580762735110ab7beab2be3b57e68de4a
Jeferson Melo <jefsdemelo@gmail.com>
2022-11-12 19:51:51 -0300

    fix(deploy): fullstack

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,10 @@
-import asyncio
-import os
-import sys
-
-import app
-
-this_files_dir = os.path.dirname(os.path.abspath(__file__))
-os.chdir(this_files_dir)
-sys.stdout.reconfigure(encoding='utf-8')
-asyncio.Server(app, host='0.0.0.0', port=8999, url_scheme='http')
+# import asyncio
+# import os
+# import sys
+#
+# import app
+#
+# this_files_dir = os.path.dirname(os.path.abspath(__file__))
+# os.chdir(this_files_dir)
+# sys.stdout.reconfigure(encoding='utf-8')
+# asyncio.Server(app, host='0.0.0.0', port=8999, url_scheme='http')
